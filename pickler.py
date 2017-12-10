69dc9c5af9a89f936df8263cbdabb9670ad2cda6
mholzel <matthew.holzel@gmail.com>
2017-12-10 13:01:46 +0100

    Moved more code out of the Ipython notebook

--- pickler.py
+++ pickler.py
@@ -1,6 +1,7 @@
-from sklearn.preprocessing import StandardScaler
 import data, os, pickle, utils
 import numpy as np
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
 
 
 def formatFeatures(features):
@@ -48,6 +49,74 @@ def loadFeatures(colorspace, channel, orient, pix_per_cell, cell_per_block, bloc
             return None
 
 
+def formatFeatures(features):
+    features = np.array(features)
+    return features.reshape(features.shape[0], -1)
+
+
+def train(clf, colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm):
+    # First, load the features
+    features = loadFeatures(colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm)
+    carTrainFeatures = formatFeatures(features['carTrain'])
+    notCarTrainFeatures = formatFeatures(features['notCarTrain'])
+
+    # Next, scale the training features
+    X = np.vstack((carTrainFeatures, notCarTrainFeatures)).astype(np.float64)
+    scaler = StandardScaler().fit(X)
+    scaled_X = scaler.transform(X)
+
+    # Define the training labels
+    y = np.hstack((np.ones(len(carTrainFeatures)), np.zeros(len(notCarTrainFeatures))))
+
+    # and fit the classifier to the data
+    clf.fit(scaled_X, y)
+
+
+def validate(clf, colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm):
+    # First, load the features
+    features = loadFeatures(colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm)
+    carTrainFeatures = formatFeatures(features['carTrain'])
+    carValidFeatures = formatFeatures(features['carValid'])
+    notCarTrainFeatures = formatFeatures(features['notCarTrain'])
+    notCarValidFeatures = formatFeatures(features['notCarValid'])
+
+    # Next, generate a scaler based on the training features
+    X = np.vstack((carTrainFeatures, notCarTrainFeatures)).astype(np.float64)
+    scaler = StandardScaler().fit(X)
+
+    # Finally, compute the score
+    X = np.vstack((carValidFeatures, notCarValidFeatures)).astype(np.float64)
+    scaled_X = scaler.transform(X)
+    y = np.hstack((np.ones(len(carValidFeatures)), np.zeros(len(notCarValidFeatures))))
+    return clf.score(scaled_X, y)
+
+
+def saveSVM(filename, colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm):
+    # Train and validate the SVM
+    clf = LinearSVC()
+    train(clf, colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm)
+
+    # Then pickle it and return it
+    pickle.dump(clf, open(filename, "wb"))
+    return clf
+
+
+def loadSVM(colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm, reload=True):
+    dir = 'pickled_svms'
+    filename = picklename(colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm)
+    path = os.path.join(dir, filename)
+    if not os.path.isfile(path):
+        # We still need to train the svm
+        # Put a placeholder file so that other threads don't start this
+        open(path, 'a').close()
+        return saveSVM(path, colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm)
+    else:
+        if reload:
+            return pickle.load(open(path, "rb"))
+        else:
+            return None
+
+
 def saveScaler(filename, colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm):
     # First, load the features
     features = loadFeatures(colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm)
@@ -79,10 +148,65 @@ def loadScaler(colorspace, channel, orient, pix_per_cell, cell_per_block, block_
             return None
 
 
-for colorspace in ['rgb', 'hsv', 'luv', 'ycrcb', 'hls']:
-    for channel in [0, 'all']:
-        for orient in [4, 8, 16]:
-            for pix_per_cell in [8, 16]:
-                for cell_per_block in [1, 2, 3]:
-                    for block_norm in ['L1']:
-                        loadScaler(colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm, False)
+class Params():
+    def __init__(self, colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm):
+        self.colorspace = colorspace
+        self.channel = channel
+        self.orient = orient
+        self.pix_per_cell = pix_per_cell
+        self.cell_per_block = cell_per_block
+        self.block_norm = block_norm
+
+    def fields(self):
+        return (self.colorspace, self.channel, self.orient, self.pix_per_cell, self.cell_per_block, self.block_norm)
+
+    def __eq__(self, rhs):
+        return self.fields() == rhs.fields()
+
+    def __hash__(self):
+        return hash(self.fields())
+
+    def __str__(self):
+        s = ''
+        s += 'colorspace: ' + self.colorspace + ', '
+        s += 'channel: ' + str(self.channel) + ', '
+        s += 'orient: ' + str(self.orient) + ', '
+        s += 'pix_per_cell: ' + str(self.pix_per_cell) + ', '
+        s += 'cell_per_block: ' + str(self.cell_per_block) + ', '
+        s += 'block_norm: ' + self.block_norm
+        return s
+
+
+def loadSVMScores():
+    # First, load our scores from file
+    filename = 'pickled_svms/scores.p'
+    if os.path.isfile(filename):
+        scores = pickle.load(open(filename, "rb"))
+    else:
+        scores = {}
+
+    # Now compute all of the scores that aren't already in the dictionary
+    for colorspace in ['rgb', 'hsv', 'luv', 'ycrcb', 'hls']:
+        for channel in [0, 'all']:
+            for orient in [4, 8, 16]:
+                for pix_per_cell in [8, 16]:
+                    for cell_per_block in [1, 2, 3]:
+                        for block_norm in ['L1']:
+                            params = Params(colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm)
+                            if params not in scores:
+                                timer = utils.Timer()
+                                svm = loadSVM(colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm)
+                                scores[params] = validate(svm, colorspace, channel, orient, pix_per_cell,
+                                                          cell_per_block, block_norm)
+                                timer.print("seconds to validate")
+                                pickle.dump(scores, open(filename, "wb"))
+    return scores
+
+if __name__ == "__main__":
+    for colorspace in ['rgb', 'hsv', 'luv', 'ycrcb', 'hls']:
+        for channel in [0, 'all']:
+            for orient in [4, 8, 16]:
+                for pix_per_cell in [8, 16]:
+                    for cell_per_block in [1, 2, 3]:
+                        for block_norm in ['L1']:
+                            loadScaler(colorspace, channel, orient, pix_per_cell, cell_per_block, block_norm, False)
--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
-import cv2, defaults, math, time
+import cv2
+import defaults
+import math
+import time
 from skimage.feature import hog
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def load(imgPath):
@@ -8,6 +13,18 @@ def load(imgPath):
         return cv2.cvtColor(cv2.imread(imgPath), cv2.COLOR_BGR2RGB)
     else:
         return imgPath
+
+
+def show_examples(dataset, title):
+    fig, axes = plt.subplots(3, 8, figsize=(16, 6.5))
+    axes = np.array(axes).ravel()
+    fig.subplots_adjust(hspace=0, wspace=0, bottom=0, left=0, top=.95, right=1)
+
+    for i, ax in enumerate(axes):
+        ax.imshow(load(dataset[i]))
+        ax.axis('off')
+    plt.suptitle(title)
+    plt.show()
 
 
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, block_norm, vis, feature_vec):
@@ -21,6 +38,35 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block, block_norm, vis,
                block_norm=block_norm,
                visualise=vis, feature_vector=feature_vec)
 
+
+def showHOGFeaturesGray(img, orients, pixs_per_cell):
+    # COnvert to grayscale so that there is only one channel
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Show the source image
+    fig, ax = plt.subplots(1, 1, figsize=(4, 4))
+    ax.imshow(gray, cmap='gray')
+    ax.axis('off')
+    ax.set_title("Source image")
+
+    fig, axes = plt.subplots(len(orients), len(pixs_per_cell), figsize=(20, 8))
+    for row, orient in enumerate(orients):
+        for col, pix_per_cell in enumerate(pixs_per_cell):
+            features, hog_image = get_hog_features(gray, orient, pix_per_cell, 2, 'L1', True, True)
+            ax = axes[row][col]
+            ax.imshow(hog_image, cmap='inferno')
+            ax.axis('off')
+            ax.set_title("Orient = " + str(orient) + ", Pix/Cell = " + str(pix_per_cell))
+
+def showHogFeatures(img, channelNames):
+    channels = img.shape[-1]
+    fig, axes = plt.subplots(1, channels, figsize=(10,10))
+    for channel in range(channels):
+            features, hog_image = get_hog_features(img[:,:,channel], 8, 8, 2, 'L1', True, True )
+            ax = axes[channel]
+            ax.imshow(hog_image, cmap = 'inferno')
+            ax.axis('off')
+            ax.set_title(channelNames[channel])
 
 def features(image,
              colorspace=defaults.colorspace(),
@@ -37,17 +83,20 @@ def features(image,
     image = rgbTo(image, colorspace=colorspace)
     if len(image.shape) < 3:
         return get_hog_features(image, orient=orient, pix_per_cell=pix_per_cell,
-                                cell_per_block=cell_per_block, vis=False, feature_vec=feature_vec, block_norm=block_norm)
+                                cell_per_block=cell_per_block, vis=False, feature_vec=feature_vec,
+                                block_norm=block_norm)
     elif channel == 'all':
         hog_features = []
         for channel in range(image.shape[2]):
             hog_features.append(
                 get_hog_features(image[:, :, channel], orient=orient, pix_per_cell=pix_per_cell,
-                                 cell_per_block=cell_per_block, vis=False, feature_vec=feature_vec, block_norm=block_norm))
+                                 cell_per_block=cell_per_block, vis=False, feature_vec=feature_vec,
+                                 block_norm=block_norm))
         return hog_features
     else:
         return get_hog_features(image[:, :, channel], orient=orient, pix_per_cell=pix_per_cell,
-                                cell_per_block=cell_per_block, vis=False, feature_vec=feature_vec, block_norm=block_norm)
+                                cell_per_block=cell_per_block, vis=False, feature_vec=feature_vec,
+                                block_norm=block_norm)
 
 
 def rgbTo(image, colorspace='rgb'):
@@ -78,7 +127,8 @@ class Timer():
 
     def reset(self):
         self.t0 = time.time()
-        
+
+
 def ipython():
     try:
         __IPYTHON__
@@ -94,4 +144,3 @@ def ystop(height, scale):
         return height
     else:
         return math.floor(height * (.66 + (scale - 1) / 3 * (1 - .66)))
-
